7777646224be9cb97865cc9639449efd187475fc
Jhonny Herrera Baldivieso <jmoprix@gmail.com>
2023-05-22 02:48:27 -0400

    Rick and Morty

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-//import ResidentsInfo from './components/ResidentsInfo'
 import ResidentInfoDos from './components/ResidentInfoDos'
 
 function App() {
@@ -16,9 +15,7 @@ function App() {
     axios
       .get(`https://rickandmortyapi.com/api/location/${idLocation}`)
       .then(resp => {
-        //console.log(resp.data)
         setLocation(resp.data)
-        //setResidents(resp.data.residents)
         getResidentsData(resp.data.residents)
       })
       .catch((error) => console.error(error))
@@ -31,7 +28,6 @@ function App() {
     axios
       .get(`https://rickandmortyapi.com/api/character/${idArray}`)
       .then(resp => {
-        //console.log(resp.data)
         setResidents(resp.data)
       })
       .catch((error) => console.error(error))
@@ -60,61 +56,80 @@ function App() {
         console.log(resp.data)
         setLocationResults(resp.data.results)
       })
-      .catch((error) =>{ 
-        console.error(error) 
+      .catch((error) => {
+        console.error(error)
         setLocationResults([])
-      })      
+      })
   }
 
   return (
-    <div>
-      <h1>{location.name}</h1>
-      <input
-        type='text'
-        onChange={e => getLocationsFiltered(e.target.value)}
-      />
-      <select
-        name='locations'
-        id='locations'
-        onChange={e => setIdLocation(e.target.value)}
-      >
-        {
-          locationResults.map((location) => (
-            <option value={location.id}>{location.name}</option>
-          ))
-        }
-      </select>
-      {/*
-        residents.map(resident => (
-          <ResidentsInfo
-            key={resident}
-            url={resident}
-          />
-        ))
-        */}
-      {
-        currenResidents.map(resident => (
-          <ResidentInfoDos
-            key={resident.id}
-            residentData={resident}
-          />
-        ))
-      }
+    <div className='todo' id='todo'>
+      <div className='head1'>
+      </div>
+      <div className='head'>
+        <h1>Rick and Morty</h1>
+      </div>
+      <div className='container'>
+        <div className='animation'>
+          <div className="fondoStars" id="fondoStars">
+            <div className='stars' id="stars"></div>
+            <div className='stars2' id="stars2"></div>
+            <div className='stars3' id="stars3"></div>
+          </div>
+          <div className="fondoStars1" id="fondoStars">
+            <div className='stars' id="stars"></div>
+            <div className='stars2' id="stars2"></div>
+            <div className='stars3' id="stars3"></div>
+          </div>
+          <div className="fondoStars2" id="fondoStars">
+            <div className='stars' id="stars"></div>
+            <div className='stars2' id="stars2"></div>
+            <div className='stars3' id="stars3"></div>
+          </div>
+          <div className='search'>
+            <h1>{location.name}</h1>
+            <input
+              type='text'
+              onChange={e => getLocationsFiltered(e.target.value)}
+            />
+            <select
+              name='locations'
+              id='locations'
+              onChange={e => setIdLocation(e.target.value)}
+            >
+              {
+                locationResults.map((location) => (
+                  <option value={location.id}>{location.name}</option>
+                ))
+              }
+            </select>
+            {
+              currenResidents.map(resident => (
+                <ResidentInfoDos
+                  key={resident.id}
+                  residentData={resident}
+                />
+              ))
+            }
 
-      <button
-        onClick={() => pagination(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        Prev
-      </button>
-      {/*[1, 2, 3].map((number) => (<button>{number}</button>))*/}
+            <button
+              onClick={() => pagination(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Prev
+            </button>
+            {/*[1, 2, 3].map((number) => (<button>{number}</button>))*/}
 
-      <button
-        onClick={() => pagination(currentPage + 1)}
-        disabled={currentPage === lastPage}
-      >
-        Next
-      </button>
+            <button
+              onClick={() => pagination(currentPage + 1)}
+              disabled={currentPage === lastPage}
+            >
+              Next
+            </button>
+
+          </div>
+        </div>
+      </div>
     </div >
   )
 }
